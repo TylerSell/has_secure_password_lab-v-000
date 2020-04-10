11d2812e7ad4e29dcc64d3431c7c30dd4d7b641f
Tyler Sell <tylerselldesign@gmail.com>
2020-04-10 20:13:19 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   
-  resources :users, only: [:new, :create]
-  
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
-  get '/welcome' => 'welcome#new'
+  post '/logout' => 'sessions#destroy'
+  
+  resources :users, only: [:new, :create]
+  
+  root 'welcome#home'
   
 end
