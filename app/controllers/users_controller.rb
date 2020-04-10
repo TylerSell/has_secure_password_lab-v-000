969a252577937fc692b8315fb6e0a538fad2f513
Tyler Sell <tylerselldesign@gmail.com>
2020-04-10 19:42:47 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,14 @@
 class UsersController < ApplicationController
+  
+  def new 
+  end 
+  
+  def create(user_params)
+  end
+  
+  private 
+  
+  def user_params
+    params.require(:user).permit(:user)
+  
 end
