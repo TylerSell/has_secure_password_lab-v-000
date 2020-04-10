b8965b6cdaaa6f0cb9c0bd2503aa383e9c35987b
Tyler Sell <tylerselldesign@gmail.com>
2020-04-10 20:06:31 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,16 @@
 class SessionsController < ApplicationController
+  
+  def new 
+  end 
+  
+  def create
+    user = User.find_by(name: params[:user][:name])
+    user = user.try(:authenticate, params[:user][:password])
+    
+  end 
+  
+  def destroy
+    session.delete :user_id
+  end 
+  
 end
